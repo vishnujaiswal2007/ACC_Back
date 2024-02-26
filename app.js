898e97c08b13ca200006acea457e9bb56675bb77
vishnujaiswal2007 <vishnujaiswal.2007@gmail.com>
2024-02-26 23:52:43 +0530

    26.02.2024

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+
+const {
+    json
+} = require('body-parser');
+const { STATUS_CODES } = require('http');
+const {
+    ObjectId
+} = require('mongodb');
+const {
+    stringify
+} = require('querystring');
+
+
 var express = require('express');
 var app = express();
 var server = app.listen(8080, function(){
@@ -6,3 +19,21 @@ var server = app.listen(8080, function(){
     console.log("Server is listening at http://%s:%s", host, port);
 });
 
+
+app.use(express.json({
+    limit: '50mb'
+}));
+
+
+app.use(express.urlencoded({
+    extended: false,
+    limit: '50mb'
+}));
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5505');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
