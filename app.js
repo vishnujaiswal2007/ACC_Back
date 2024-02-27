5f91323ef7fb624eee681d2286d866a8e55aa757
vishnujaiswal2007 <vishnujaiswal.2007@gmail.com>
2024-02-27 23:42:47 +0530

    27.02.2024

--- app.js
+++ app.js
@@ -12,8 +12,9 @@ const {
 
 
 var express = require('express');
+const { url } = require('inspector');
 var app = express();
-var server = app.listen(8080, function(){
+var server = app.listen(8081, function(){
     var host = server.address().address
     var port = server.address().port
     console.log("Server is listening at http://%s:%s", host, port);
@@ -36,4 +37,25 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
+});
+
+
+app.post('/formsubmit', function(req,res,next){
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://vishnu:acc%402022@localhost:27017/";
+    MongoClient.connect(url, function(err, db){
+        if(err) throw err;
+        var dbo = db.db("ACC");
+        dbo.collection("users").count({}, function(error, numofDocs){
+            if(error) return callback(error);
+            else{
+                var myobj = req.body;
+                dbo.collection("users").insertOne(myobj, function(err, res){
+                    if(err) throw err;
+                    db.close
+                });
+                res.send("1");
+            }
+        })
+    })
 });
