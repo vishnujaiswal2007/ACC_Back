0e1078ec630cc8afea7002a581fbba51e56ee07f
Vishnu Jaiswal <vishnujaiswal.2007@gmail.com>
2025-01-01 14:46:35 +0530

    ACC SMS dated 01.01.2025

---
+++ routes/userRouters.js
@@ -0,0 +1,18 @@
+import express from 'express'
+const router = express.Router()
+import userController from '../controller/userController.js'
+
+//Authentication
+
+
+//Public Roters
+router.post('/registration', userController.userRegistration)
+router.post('/login', userController.login)
+router.post('/UG_details', userController.getdetails)
+router.post('/getcourse', userController.getcourse)
+
+//Private Routers
+router.post('/chngpas', userController.changepassword)
+
+
+export default router
